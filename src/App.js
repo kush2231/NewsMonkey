a70f1880086e5780c080a9e974736cb546356691
kush <kushagra885346@gmail.com>
2023-07-22 19:44:59 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,155 @@
 import "./App.css";
-import React, { Component } from "react";
-import Navbar from "./Components/Navbar";
+import React, { useState } from "react";
+import NavBar from "./Components/NavBar";
 import News from "./Components/News";
-export default class App extends Component {
-  c = "john";
-  render() {
-    return (
-      <div>
-        <Navbar />
-        <News />
-      </div>
-    );
-  }
-}
+import {
+  BrowserRouter ,
+  Route,
+  Outlet,
+  Routes,
+} from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
+import apiKey from "./config";
+const App = () => {
+  const pageSize = 5;
+  // console.log(apiKey);
+  const [progress, setProgress] = useState(0);
+
+  return (
+    <div className="App"  >
+      <BrowserRouter BrowserRouter>
+        <Routes>
+          <Route
+            element={
+              <div>
+                <NavBar />
+                <LoadingBar height={3} color='green' progress={progress} />
+                <Outlet/>
+              </div>
+            }
+
+          >
+            <Route
+              exact
+              path='/'
+              element={
+                <>
+                  <News
+                    setProgress={setProgress}
+                    apiKey={apiKey}
+                    key='general'
+                    pageSize={pageSize}
+                    country='in'
+                    category='general'
+                  />
+                </>
+              }
+            />
+            <Route
+              exact
+              path='/business'
+              element={
+                <News
+                  setProgress={setProgress}
+                  apiKey={apiKey}
+                  key='business'
+                  pageSize={pageSize}
+                  country='in'
+                  category='business'
+                />
+              }
+            />
+
+            <Route
+              exact
+              path='/entertainment'
+              element={
+                <News
+                  setProgress={setProgress}
+                  apiKey={apiKey}
+                  key='entertainment'
+                  pageSize={pageSize}
+                  country='in'
+                  category='entertainment'
+                />
+              }
+            />
+            <Route
+              exact
+              path='/general'
+              element={
+                <News
+                  setProgress={setProgress}
+                  apiKey={apiKey}
+                  key='general'
+                  pageSize={pageSize}
+                  country='in'
+                  category='general'
+                />
+              }
+            />
+            <Route
+              exact
+              path='/health'
+              element={
+                <News
+                  setProgress={setProgress}
+                  apiKey={apiKey}
+                  key='health'
+                  pageSize={pageSize}
+                  country='in'
+                  category='health'
+                />
+              }
+            />
+            <Route
+              exact
+              path='/science'
+              element={
+                <News
+                  setProgress={setProgress}
+                  apiKey={apiKey}
+                  key='science'
+                  pageSize={pageSize}
+                  country='in'
+                  category='science'
+                />
+              }
+            />
+
+            <Route
+              exact
+              path='/sports'
+              element={
+                <News
+                  setProgress={setProgress}
+                  apiKey={apiKey}
+                  key='sports'
+                  pageSize={pageSize}
+                  country='in'
+                  category='sports'
+                />
+              }
+            />
+            <Route
+              exact
+              path='/technology'
+              element={
+                <News
+                  setProgress={setProgress}
+                  apiKey={apiKey}
+                  key='technology'
+                  pageSize={pageSize}
+                  country='in'
+                  category='technology'
+                />
+              }
+            />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,31 +1,50 @@
-import React, { Component } from 'react'
+import React from "react";
 
-export class NewsItem extends Component {
-  render() {
-    let { title, description, ImageUrl, newsUrl } = this.props;
-    return (
-      <div className='my-3'>
-        <div className='card' style={{ width: "18rem" }}>
-          <img
-            src={
-              ImageUrl
-                ? ImageUrl
-                : "https://www.livemint.com/lm-img/img/2023/07/19/600x338/The-country-has-received-340-8-mm-of-monsoon-rains_1689767634278.jpg"
-            }
-            className='card-img-top'
-            alt='...'
-          />
-          <div className='card-body'>
-            <h5 className='card-title'>{title}..</h5>
-            <p className='card-text'>{description}...</p>
-            <a href={newsUrl} target='blank' className='btn btn-sm btn-primary'>
-              Read More
-            </a>
-          </div>
+const NewsItem = (props) => {
+  let { title, description, imageUrl, newsUrl, author, date, source } = props;
+  return (
+    <div className='my-3'>
+      <div className='card'>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            position: "absolute",
+            right: "0",
+          }}
+        >
+          <span className='badge rounded-pill bg-danger'> {source} </span>
+        </div>
+        <img
+          src={
+            !imageUrl
+              ? "https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg"
+              : imageUrl
+          }
+          className='card-img-top'
+          alt='...'
+        />
+        <div className='card-body'>
+          <h5 className='card-title'>{title} </h5>
+          <p className='card-text'>{description}</p>
+          <p className='card-text'>
+            <small className='text-muted'>
+              By {!author ? "Unknown" : author} on{" "}
+              {new Date(date).toGMTString()}
+            </small>
+          </p>
+          <a
+            rel='noreferrer'
+            href={newsUrl}
+            target='_blank'
+            className='btn btn-sm btn-dark'
+          >
+            Read More
+          </a>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
-export default NewsItem
+export default NewsItem;
